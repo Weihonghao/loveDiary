a5055cf0321f656a323f612f1526e9a4b992207c
Weihonghao <SCIN@dn0a2295d3.sunet>
2017-03-14 19:29:45 -0700

    add search bar

--- loveDiary/TweetContactTableViewController.swift
+++ loveDiary/TweetContactTableViewController.swift
@@ -10,27 +10,56 @@ import UIKit
 import CoreData
 
 class TweetContactTableViewController: FetchedResultsTableViewController, UISearchBarDelegate, UITextFieldDelegate {
-
     
     
-    @IBOutlet weak var searchTextField: UITextField!{
-        didSet {
-            searchTextField.delegate = self
-        }
-    }
-
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        if textField == searchTextField {
-            queryText = searchTextField.text
-        }
-        return true
-    }
+    
+    /*@IBOutlet weak var searchTextField: UITextField!{
+     didSet {
+     searchTextField.delegate = self
+     }
+     }
+     
+     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+     if textField == searchTextField {
+     queryText = searchTextField.text
+     }
+     return true
+     }*/
+    
     
     var queryText: String? {
         didSet {
             print("start updating UI")
             updateUI()
         }
+    }
+    @IBOutlet weak var searchBar: UISearchBar!{
+        didSet {
+            searchBar.delegate = self
+            
+            searchBar.autocapitalizationType = .none
+        }
+    }
+    
+    public func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.showsScopeBar = true
+        searchBar.sizeToFit()
+        searchBar.setShowsCancelButton(true, animated: true)
+        
+        return true
+    }
+    
+    public func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.showsScopeBar = false
+        searchBar.sizeToFit()
+        searchBar.setShowsCancelButton(false, animated: true)
+        
+        return true
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        queryText = searchBar.text
+        print("query is \(queryText)")
     }
     
     var fetchedResultsController: NSFetchedResultsController<UserData>?
@@ -40,6 +69,7 @@ class TweetContactTableViewController: FetchedResultsTableViewController, UISear
     
     private func updateUI() {
         print("start updating ui")
+        //print("\(self.searchBar.selectedScopeButtonIndex)")
         if let context = container?.viewContext, queryText != nil {
             let request: NSFetchRequest<UserData> = UserData.fetchRequest()
             // For extra credit 1, in order to get two different sections, we need to add this new sortDescriptors as label
@@ -51,7 +81,13 @@ class TweetContactTableViewController: FetchedResultsTableViewController, UISear
                 )]
             
             //popular things happen more than 1
-            request.predicate = NSPredicate(format: "screenName = %@", queryText!)
+            let selectIndex = self.searchBar.selectedScopeButtonIndex
+            if selectIndex == 0 {
+                request.predicate = NSPredicate(format: "screenName = %@", queryText!)
+            } else if selectIndex == 1 {
+                request.predicate = NSPredicate(format: "tweetName = %@", queryText!)
+            }
+            
             //request.predicate = nil
             
             
@@ -93,11 +129,11 @@ class TweetContactTableViewController: FetchedResultsTableViewController, UISear
         super.viewDidLoad()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -123,7 +159,7 @@ class TweetContactTableViewController: FetchedResultsTableViewController, UISear
             }
         }
     }
-
+    
     @IBAction func unwindToRoot(sender: UIStoryboardSegue) { }
     override func canPerformUnwindSegueAction(_ action: Selector, from fromViewController: UIViewController, withSender sender: Any) -> Bool {
         //print("try here")
@@ -137,60 +173,60 @@ class TweetContactTableViewController: FetchedResultsTableViewController, UISear
         return false
     }
     // MARK: - Table view data source
-
+    
     /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     
+     // Configure the cell...
+     
+     return cell
+     }
+     */
+    
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
